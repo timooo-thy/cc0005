4feda549abc76ca5ffec81ee7f2880dcf816325a
Timothy Lee <106719865+timooo-thy@users.noreply.github.com>
2023-04-09 14:39:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,8 @@ function initMap() {
       });
 }
 
-
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 
 
